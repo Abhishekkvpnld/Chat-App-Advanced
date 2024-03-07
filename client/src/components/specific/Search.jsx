5072b6a2ce7d07f54ac54bae42f1adef26318d2a
Abhishekkv <abhishekkvpnld@gmail.com>
2024-03-07 16:04:16 +0530

    search component created

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { useInputValidation } from '6pp';
+import { Search as SearchIcon } from "@mui/icons-material";
+import UserItem from '../shared/userItem';
+import { SampleUsers } from '../../constants/SampleChat';
+
 
 const Search = () => {
+
+  const search = useInputValidation('')
+
+let isLoadingSendFriendRequest = false;
+const [user, setuser] = useState(SampleUsers)
+
+const addFriendHandler = (id)=>{
+  console.log(id);
+}
+
   return (
-    <div>Search</div>
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"} >Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.valuer}
+          onChange={search.changeHandler}
+          variant='outlined'
+          size='small'
+          InputProps={
+            {
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }
+          }
+        />
+
+<List>
+{
+  user.map((user)=>(
+  <UserItem
+  user={user} key={user._id} 
+  handler={addFriendHandler}
+  handlerIsLoading={isLoadingSendFriendRequest}
+  />
+  ))
+}
+</List>
+
+      </Stack>
+    </Dialog>
   )
 }
 
--- client/src/constants/SampleChat.js
+++ client/src/constants/SampleChat.js
@@ -14,3 +14,19 @@ export const SampleChat = [
             members:["1","2"]
         },
 ]
+
+
+export const SampleUsers = [
+    {
+        avatar:["https://cdn.vectorstock.com/i/preview-1x/17/61/male-avatar-profile-picture-vector-10211761.jpg"],
+        name:"Abhishek",
+        _id:"1",
+      
+    },
+    {
+        avatar:["https://cdn.vectorstock.com/i/preview-1x/17/61/male-avatar-profile-picture-vector-10211761.jpg"],
+        name:"Abhishek pnld",
+        _id:"2",
+      
+    },
+]
