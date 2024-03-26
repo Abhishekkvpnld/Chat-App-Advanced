1b621212da33014a0f2543d97ecdd548d1fd509b
Abhishekkv <abhishekkvpnld@gmail.com>
2024-03-26 13:42:36 +0530

    chat routes validate completed

--- server/lib/validators.js
+++ server/lib/validators.js
@@ -1,32 +1,70 @@
 
-import { body, check, validationResult } from "express-validator";
+import { body, check, validationResult, param, query } from "express-validator";
 import { ErrorHandler } from "../utils/utility.js";
-
-
-export const registerValidator = () => [
-
-    body("name", "Please Enter Name").notEmpty(),
-    body("username", "Please Enter Username").notEmpty(),
-    body("password", "Please Enter Password").notEmpty(),
-    body("bio", "Please Enter Bio").notEmpty(),
-    check("avatar","Please Upload Avatar").notEmpty()
-
-];
-
-export const loginValidator = () => [
-
-    body("username", "Please Enter Username").notEmpty(),
-    body("password", "Please Enter Password").notEmpty(),
-
-];
 
 export const validateHandler = (req, res, next) => {
 
     const error = validationResult(req);
 
-    const errorMessages = error.array().map((error)=>error.msg).join(",")
+    const errorMessages = error.array().map((error) => error.msg).join(",")
 
-    if(error.isEmpty()) return next()
-    else return next(new ErrorHandler(errorMessages,400))
+    if (error.isEmpty()) return next()
+    else return next(new ErrorHandler(errorMessages, 400))
 
 };
+
+
+export const registerValidator = () => [
+    body("name", "Please Enter Name").notEmpty(),
+    body("username", "Please Enter Username").notEmpty(),
+    body("password", "Please Enter Password").notEmpty(),
+    body("bio", "Please Enter Bio").notEmpty(),
+    check("avatar", "Please Upload Avatar").notEmpty()
+];
+
+
+export const loginValidator = () => [
+    body("username", "Please Enter Username").notEmpty(),
+    body("password", "Please Enter Password").notEmpty(),
+];
+
+
+
+export const newGroupChatValidator = () => [
+    body("name", "Please Enter Name").notEmpty(),
+    body("members").notEmpty().withMessage("Please Enter Members")
+        .isArray({ min: 2, max: 100 }).withMessage("Members must be 2-100")
+];
+
+
+export const addMemberValidator = () => [
+    body("chatId", "Please Enter chat ID").notEmpty(),
+    body("members").notEmpty().withMessage("Please Enter Members")
+        .isArray({ min: 1, max: 97 }).withMessage("Members must be 1-97")
+];
+
+
+export const removeMemberValidator = () => [
+    body("chatId", "Please Enter Chat ID").notEmpty(),
+    body("userId", "Please Enter User ID").notEmpty(),
+];
+
+
+export const chatIdValidator = () => [
+    param("id", "Please Enter Chat ID").notEmpty()
+];
+
+
+export const sendAttachmentsValidator = () => [
+    body("chatId", "Please Enter Chat ID").notEmpty(),
+    check("files").notEmpty().withMessage("Please Upload Attachments")
+        .isArray({ min: 1, max: 5 }).withMessage("Attachments must be 1-5")
+];
+
+export const renameGroupValidator = () => [
+    param("id", "Please Enter Chat ID").notEmpty(),
+    body("name", "Please Enter New Name").notEmpty()
+];
+
+
+
--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { addMembers, deleteChat, getChatDetails, getMessages, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMembers, renameGroup, sendAttachments } from "../controllers/chatControllers.js";
 import { AttachmentsMulter } from "../middlewares/multerUpload.js";
+import { addMemberValidator, chatIdValidator, newGroupChatValidator, removeMemberValidator, renameGroupValidator, sendAttachmentsValidator, validateHandler } from "../lib/validators.js"
 
 const app = express.Router();
 
@@ -10,19 +11,22 @@ const app = express.Router();
 //user must be logged in to access the routes
 app.use(isAuthenticated);
 
-app.post("/new", newGroupChat);
+app.post("/new", newGroupChatValidator(), validateHandler, newGroupChat);
 app.get("/my", getMyChats);
 app.get("/my/groups", getMyGroups);
-app.put("/addmembers", addMembers);
-app.put("/removemember", removeMembers);
-app.delete("/leave/:id", leaveGroup);
+app.put("/addmembers", addMemberValidator(), validateHandler, addMembers);
+app.put("/removemember", removeMemberValidator(), validateHandler, removeMembers);
+app.delete("/leave/:id", chatIdValidator(), validateHandler, leaveGroup);
 
 //Send Attachments
-app.post("/messages", AttachmentsMulter, sendAttachments);
+app.post("/messages", AttachmentsMulter, sendAttachmentsValidator(), validateHandler, sendAttachments);
 //Get Messages
-app.get("/messages/:id",getMessages);
+app.get("/messages/:id", chatIdValidator(), validateHandler, getMessages);
 //Get Chat details, Rename, Delete
-app.route("/:id").get(getChatDetails).put(renameGroup).delete(deleteChat);
+app.route("/:id")
+    .get(chatIdValidator(),validateHandler,getChatDetails)
+    .put( renameGroupValidator(),validateHandler,renameGroup)
+    .delete(chatIdValidator(),validateHandler,deleteChat);
 
 
 export default app;   
